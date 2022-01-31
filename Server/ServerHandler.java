3f1ac77568a156d3414a7df426be1d8d00b4c7f8
AhmedAbdelaty2 <ahmedabdelaty2021@gmail.com>
2022-02-01 01:01:59 +0200

    updated

--- Server/ServerHandler.java
+++ Server/ServerHandler.java
@@ -67,6 +67,7 @@ class ServerHandler extends Thread
                 }
             }catch(IOException ex){
                 //ex.printStackTrace();
+                loggedPlayers.remove(this);
             }
         }
     }
