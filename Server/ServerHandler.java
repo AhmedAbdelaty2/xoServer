a391543f25e9e7c214f6aa3d2b79234aa9d76bb4
AhmedAbdelaty2 <ahmedabdelaty2021@gmail.com>
2022-01-31 21:39:25 +0200

    invite feature is added

--- Server/ServerHandler.java
+++ Server/ServerHandler.java
@@ -15,25 +15,19 @@ class ServerHandler extends Thread
 
     public ServerHandler(Socket cs, DBConnection db1)
     {
-        try
-        {
+        try{
             db = db1;
             dis = new DataInputStream(cs.getInputStream());
             ps= new PrintStream(cs.getOutputStream());
             start();
-        }
-        catch(IOException ex)
-        {
+        }catch(IOException ex){
             ex.printStackTrace();
         }
     }
 
-    public void run()
-    {
-        while(true)
-        {
-            try
-            {
+    public void run(){
+        while(true){
+            try{
                 String inData= dis.readLine();
                 String state = inData.split("\\*")[0];
 
@@ -70,24 +64,22 @@ class ServerHandler extends Thread
                         break;
 
                 }
-            }
-            catch(IOException ex)
-            {
+            }catch(IOException ex){
                 //ex.printStackTrace();
             }
         }
     }
 
-    void signInHandler(String data) {
+    void signInHandler(String data){
         String[] arrData = data.split("\\*");
 
-        try {
+        try{
             boolean falg = true;
-            if (loggedPlayers.isEmpty()) {
+            if (loggedPlayers.isEmpty()){
                 handle = db.signIn(arrData[1], arrData[2]);
-            } else {
-                for (ServerHandler s : loggedPlayers) {
-                    if (s.playerName.equals(arrData[1])) {
+            }else{
+                for (ServerHandler s : loggedPlayers){
+                    if (s.playerName.equals(arrData[1])){
                         handle = "dublicated";
                         falg = false;
                         break;  
@@ -97,7 +89,7 @@ class ServerHandler extends Thread
                     handle = db.signIn(arrData[1], arrData[2]);
             }
 
-            switch (handle) {
+            switch (handle){
                 case "pass":
                     this.playerName = arrData[1];
                     this.playerScore = db.getScore(this.playerName);
@@ -118,25 +110,25 @@ class ServerHandler extends Thread
                     ps.println("wrong");
                     break;
             }
-        } catch (ArrayIndexOutOfBoundsException AI) {
+        }catch(ArrayIndexOutOfBoundsException AI){
             ps.println("wrong");
         } 
     }
 
-    void signUpHandler(String data) {
+    void signUpHandler(String data){
         String[] arrData = data.split("\\*");
         System.out.println(arrData[0]);
 
-        try {
-            if (db.signUp(arrData[1], arrData[2], arrData[3])) {
+        try{
+            if (db.signUp(arrData[1], arrData[2], arrData[3])){
                 this.playerName = arrData[1];
                 //this.playerScore = db.getScore(this.playerName);
                 loggedPlayers.add(this);
                 ps.println("done");
-            } else {
+            }else{
                 ps.println("failed");
             }
-        } catch (ArrayIndexOutOfBoundsException AI) {
+        }catch(ArrayIndexOutOfBoundsException AI){
             ps.println("wrong");
         }   
     }
@@ -159,30 +151,30 @@ class ServerHandler extends Thread
         }
     }
 
-    void invite(String data) throws IOException {
+    void invite(String data) throws IOException{
         String[] receivedData = data.split("\\*");
         player1name = receivedData[1];
         player2name = receivedData[2];
-        for (ServerHandler s : loggedPlayers) {
-            if (s.playerName.equals(player2name)) {
+        for(ServerHandler s : loggedPlayers){
+            if(s.playerName.equals(player2name)){
                 s.ps.println("request*"+player1name);
             }
         }
     }
 
-    private void handleRequest(String req) {
-        switch (req) {
+    private void handleRequest(String req){
+        switch (req){
             case "ok":
-                for (ServerHandler s : loggedPlayers) {
-                    if (s.playerName.equals(player1name)) {
+                for(ServerHandler s : loggedPlayers){
+                    if(s.playerName.equals(player1name)){
                         s.ps.println("start*"+player2name+"*"+player1name);
                     }
                 }
                  //startMatch();
                 break;
             case "refused":
-                for (ServerHandler s : loggedPlayers) {
-                    if (s.playerName.equals(player1name)) {
+                for(ServerHandler s : loggedPlayers){
+                    if(s.playerName.equals(player1name)){
                         s.ps.println("refused*");
                     }
                 }
